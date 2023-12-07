cf4990d690ae5d7c7839f5417d08ad08075d2e0f
Sarada Ganta <saradaganta@saradas-mbp.attlocal.net>
2023-12-07 13:49:55 -0600

    serverscript to build srever

---
+++ server-script.sh
@@ -0,0 +1,15 @@
+sudo yum install java -y
+sudo yum install maven -y
+sudo yum install git -y
+
+if [ -d "addressbook" ]
+then
+echo "repo is the exists and pulling"
+cd /home/ec2-user/addressbook
+git pull origin hello
+else
+echo "repo is not exists"
+git clone https://github.com/sarasivavikku/addressbook.git
+cd /home/ec2-user/addressbook
+fi 
+mvn package
