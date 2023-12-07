f12383f571ee6c1435ce89bf27a1c176a90a4243
sarasivavikku <152935127+sarasivavikku@users.noreply.github.com>
2023-12-07 15:20:08 -0600

    Create server-script.sh

---
+++ server-script.sh
@@ -0,0 +1,15 @@
+sudo yum install java-1.8.0-openjdk-devel -y 
+sudo yum install maven -y
+sudo yum install git -y
+
+if [ -d "addressbook" ]
+then
+echo "repo is the exists and pulling"
+cd /home/ec2-user/addressbook
+git pull origin Sit_pipeline
+else
+echo "repo is not exists"
+git clone https://github.com/sarasivavikku/addressbook.git
+cd /home/ec2-user/addressbook
+fi 
+mvn package
