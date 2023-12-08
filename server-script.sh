01501f902878fafec41e15adf50774ca10590f28
sarasivavikku <152935127+sarasivavikku@users.noreply.github.com>
2023-12-08 09:59:27 -0600

    Update server-script.sh

--- server-script.sh
+++ server-script.sh
@@ -5,11 +5,11 @@
 if [ -d "addressbook" ]
 then
 echo "repo is the exists and pulling"
-cd /home/ec2-user/addressbook
+cd /home/ec2-user/addressbook/
 git pull origin Sit_pipeline
 else
 echo "repo is not exists"
 git clone https://github.com/sarasivavikku/addressbook.git
-cd /home/ec2-user/addressbook
+cd /home/ec2-user/addressbook/
 fi 
 mvn package
